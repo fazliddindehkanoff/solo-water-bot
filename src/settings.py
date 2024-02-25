8779e60737754079070c1979dbbfa56f028b532f
DehqonovFazliddin <fazliddindehkanoff@yandex.com>
2024-02-26 04:07:02 +0500

    last work

--- src/settings.py
+++ src/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # external apps
     "django_lifecycle_checks",
-    "search_placeholder",
     # local apps
     "tgbot",
 ]
@@ -125,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
